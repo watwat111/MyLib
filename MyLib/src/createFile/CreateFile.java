f9f9def92520d9cbc9d560b2712ad6961cbf0db0
wat111 <watwat1253@gmail.com>
2015-07-12 17:08:52 +0900

    作成

---
+++ MyLib/src/createFile/CreateFile.java
@@ -0,0 +1,24 @@
+package createFile;
+
+import java.io.File;
+import java.util.Calendar;
+
+public class CreateFile {
+	
+	public static String createFileByData(String masterPath,String subPath){
+		Calendar now = Calendar.getInstance();
+		String saveFilePath = masterPath + "\\" + now.get(Calendar.YEAR) + "_" + (now.get(Calendar.MONTH) + 1) + "_"
+				+ now.get(Calendar.DAY_OF_MONTH) + subPath;
+
+		File newdir = new File(saveFilePath);
+		if (newdir.exists()) {
+
+		} else {
+			newdir.mkdirs();
+		}
+		
+		return saveFilePath;
+	}
+	
+
+}
